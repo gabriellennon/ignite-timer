383593f7c5f1c9b5fea90142a4685d7e5e72644e
frontend <gabriellennon@wizsolucoes.com.br>
2022-10-22 12:36:15 -0300

    configure es-lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from './styles/globa';
-import { defaultTheme } from './styles/themes/default';
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/globa'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
