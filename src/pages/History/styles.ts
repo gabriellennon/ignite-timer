fc0649a24f6fddbfaa79a5022f4a92de6c403655
frontend <gabriellennon@wizsolucoes.com.br>
2023-01-09 18:47:05 -0300

    style status table

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -59,3 +59,28 @@ export const HistoryList = styled.div`
     }
   }
 `
+// as const = estamos dizendo pro TS que nao é só um texto, vai ser sempre um dos 3
+const STATUS_COLORS = {
+  yellow: 'yellow-500',
+  green: 'green-500',
+  red: 'red-500',
+} as const
+
+// Dizendo que minhas cores vao ser as chaves do meu STATUS_COLORS
+interface StatusProps {
+  statusColor: keyof typeof STATUS_COLORS
+}
+
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: '';
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 50%;
+    background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
+  }
+`
